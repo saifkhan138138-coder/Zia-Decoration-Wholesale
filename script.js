ab9a5c4009af08f5d34a5b10e7148ff3f4a19158
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-07-18 09:49:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
+const cards = document.querySelectorAll(".card");
 
+const selectedItems = document.getElementById("selectedItems");
+const totalQty = document.getElementById("totalQty");
+
+let selected = 0;
+let quantity = 0;
+
+cards.forEach(card => {
+
+const minus = card.querySelectorAll("button")[0];
+const plus = card.querySelectorAll("button")[1];
+
+const number = card.querySelector("span");
+const tick = card.querySelector(".tick");
+
+let qty = 0;
+
+plus.onclick = () => {
+
+qty++;
+
+number.innerText = qty;
+
+quantity++;
+
+if(qty==1){
+
+selected++;
+
+tick.style.display="block";
+
+card.style.border="2px solid gold";
+
+}
+
+selectedItems.innerText=selected;
+
+totalQty.innerText=quantity;
+
+};
+
+minus.onclick = () => {
+
+if(qty>0){
+
+qty--;
+
+number.innerText=qty;
+
+quantity--;
+
+if(qty==0){
+
+selected--;
+
+tick.style.display="none";
+
+card.style.border="2px solid #333";
+
+}
+
+selectedItems.innerText=selected;
+
+totalQty.innerText=quantity;
+
+}
+
+};
+
+});
