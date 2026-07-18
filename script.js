6eb8fede428d6bdb2d58402ec496980cd9ff72da
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-07-18 15:49:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,131 +5,170 @@ const totalQty = document.getElementById("totalQty");
 
 let selected = 0;
 let quantity = 0;
-let orderList = {};
+
+
 cards.forEach(card => {
 
 const minus = card.querySelectorAll("button")[0];
 const plus = card.querySelectorAll("button")[1];
 
-const number = card.querySelector("span");
+const number = card.querySelector(".qty span");
 const tick = card.querySelector(".tick");
 
 let qty = 0;
+
 const productNo = card.querySelector("h3").innerText;
+
+
 plus.onclick = () => {
 
 qty++;
 
 number.innerText = qty;
-orderList[productNo] = qty;
+
 quantity++;
 
-if(qty==1){
+
+if(qty == 1){
 
 selected++;
 
-tick.style.display="block";
+tick.style.display = "block";
 
-card.style.border="2px solid gold";
+card.style.border = "2px solid gold";
 
 }
 
-selectedItems.innerText=selected;
 
-totalQty.innerText=quantity;
+selectedItems.innerText = selected;
+
+totalQty.innerText = quantity;
 
 };
+
+
 
 minus.onclick = () => {
 
-if(qty>0){
+if(qty > 0){
 
 qty--;
 
-number.innerText=qty;
-if(qty==0){
-delete orderList[productNo];
-}else{
-orderList[productNo]=qty;
-  }
-console.log(orderList);
+number.innerText = qty;
+
 quantity--;
 
-if(qty==0){
+
+if(qty == 0){
 
 selected--;
 
-tick.style.display="none";
+tick.style.display = "none";
 
-card.style.border="2px solid #333";
+card.style.border = "2px solid #333";
 
 }
 
-selectedItems.innerText=selected;
 
-totalQty.innerText=quantity;
+selectedItems.innerText = selected;
+
+totalQty.innerText = quantity;
 
 }
 
 };
 
+
 });
+
+
+
 const orderBtn = document.getElementById("orderBtn");
+
 
 orderBtn.onclick = () => {
 
+
 const name = document.getElementById("customerName").value.trim();
+
 const phone = document.getElementById("customerPhone").value.trim();
+
 const city = document.getElementById("customerCity").value.trim();
+
 const address = document.getElementById("customerAddress").value.trim();
 
+
+
 if(name=="" || phone=="" || city==""){
+
 alert("Please fill Name, Mobile Number and City.");
+
 return;
+
 }
 
-let message = "Assalam-o-Alaikum%0A%0A";
 
-message += "*Customer Details*%0A";
-message += "Name: " + name + "%0A";
-message += "Phone: " + phone + "%0A";
-message += "City: " + city + "%0A";
+
+let message = 
+"Assalam-o-Alaikum\n\n";
+
+
+message += "*Customer Details*\n";
+
+message += "Name: " + name + "\n";
+
+message += "Phone: " + phone + "\n";
+
+message += "City: " + city + "\n";
+
 
 if(address!=""){
-message += "Address: " + address + "%0A";
+
+message += "Address: " + address + "\n";
+
 }
 
-message += "%0A";
-message += "*Order Details*%0A";
+
+message += "\n*Order Details*\n";
+
 
 
 cards.forEach(card => {
 
+
 let product = card.querySelector("h3").innerText;
+
 
 let qty = parseInt(
 card.querySelector(".qty span").innerText
 );
 
+
+
 if(qty > 0){
 
-message += product + " × " + qty + "%0A";
+message += product + " × " + qty + "\n";
 
 }
+
 
 });
 
 
-message += "%0A";
 
-message += "Selected Items: " + selectedItems.innerText + "%0A";
+message += "\nSelected Items: " + selectedItems.innerText;
 
-message += "Total Quantity: " + totalQty.innerText + "%0A%0A";
+message += "\nTotal Quantity: " + totalQty.innerText;
 
 
-window.open(
-"https://wa.me/923456611435?text=" + message,
-"_blank"
-);
+
+let whatsappURL = 
+"https://wa.me/923456611435?text=" 
++ encodeURIComponent(message);
+
+
+
+window.open(whatsappURL, "_blank");
+
 
 };
