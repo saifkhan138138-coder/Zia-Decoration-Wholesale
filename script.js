ea5ef8dc810b6f11729f57c44e05181b6a8c0ae0
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-07-18 12:41:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,39 @@ totalQty.innerText=quantity;
 };
 
 });
+const orderBtn = document.getElementById("orderBtn");
+
+orderBtn.onclick = () => {
+
+const name = document.getElementById("customerName").value.trim();
+const phone = document.getElementById("customerPhone").value.trim();
+const city = document.getElementById("customerCity").value.trim();
+const address = document.getElementById("customerAddress").value.trim();
+
+if(name=="" || phone=="" || city==""){
+alert("Please fill Name, Mobile Number and City.");
+return;
+}
+
+let message = "Assalam-o-Alaikum%0A%0A";
+
+message += "*Customer Details*%0A";
+message += "Name: " + name + "%0A";
+message += "Phone: " + phone + "%0A";
+message += "City: " + city + "%0A";
+
+if(address!=""){
+message += "Address: " + address + "%0A";
+}
+
+message += "%0A";
+
+message += "Selected Items: " + selected + "%0A";
+message += "Total Quantity: " + quantity + "%0A%0A";
+
+window.open(
+"https://wa.me/923402000374?text=" + message,
+"_blank"
+);
+
+};
