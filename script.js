9c19ab885806ff79474a051838f530d24bdc9156
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-07-18 13:17:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -98,12 +98,19 @@ if(address!=""){
 message += "Address: " + address + "%0A";
 }
 
-message += "%0A";
 message += "*Order Details*%0A";
 
-Object.keys(orderList).forEach(function(product){
+cards.forEach(card=>{
 
-message += product + " × " + orderList[product] + "%0A";
+const product = card.querySelector("h3").innerText;
+
+const qty = parseInt(card.querySelector("span").innerText);
+
+if(qty>0){
+
+message += product + " × " + qty + "%0A";
+
+}
 
 });
 
